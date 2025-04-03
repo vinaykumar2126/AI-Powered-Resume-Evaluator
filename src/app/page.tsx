79c270202b9acf41fece85e03d76d7fd42a2707b
vgodavarti <vgodavarti@binghamton.edu>
2025-04-02 21:21:54 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-gray-900 text-white text-2xl">
+    Resume Evaluator is Live! 🚀
+  </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     darkMode: "class", // Enables manual dark mode toggle support
+//     theme: {
+//       extend: {
+//         colors: {
+//           background: "var(--background)",
+//           foreground: "var(--foreground)",
+//         },
+//       },
+//     },
+//     plugins: [],
+//   };
+  
